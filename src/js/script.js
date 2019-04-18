e26f2fe2d4fb88e0ae0dac005c5502c388863fa1
Rifat Hossain <admin@rhossain.me>
2019-04-18 13:58:49 +0600

    animatejs added

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,24 @@ import "../scss/bundle.scss";
 
 
 
-let $ = require('jquery');
+window.$ = window.jQuery = require('jquery');
+require('animatescroll.js');
 require('popper.js');
 require('bootstrap');
 require('@fortawesome/fontawesome-free/js/all');
+
+
+// smooth scroll
+// $(function () {
+//     $('.nav-link').click(function (e) {
+//         e.preventDefault();
+//         var thisLink = $(this).attr('href');
+//         $('html, body').animate({
+//             scrollTop: $(thisLink).offset().top
+//         },1000);
+
+//     });
+// });
+
+// animate scroll
+// $('#a').animatescroll({element:'#service',padding:20});
